ee91b06db1fed250a0a5a73f293435aec0669253
jason <saga1654@kakao.com>
2024-04-19 10:41:55 +0900

    splash ui

---
+++ iosApp/iosApp/SplashScreen.swift
@@ -0,0 +1,79 @@
+//
+//  SplashScreen.swift
+//  iosApp
+//
+//  Created by jason on 4/19/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    @State var splashLoading = false
+    @State var logoPaddingBottom: CGFloat = 0
+    var body: some View {
+        ZStack {
+            VStack {
+                Spacer()
+                Text("coinbase")
+                    .padding(.bottom, logoPaddingBottom)
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 28))
+                    .fontWeight(.bold)
+                    
+                Spacer()
+            }
+            VStack {
+                Spacer()
+                
+                if splashLoading {
+                    Button("시작하기", action: {
+                        print("OK")
+                    })
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(3.0)
+                        .transition(.move(edge: .bottom).combined(with: .opacity))
+                    Text("로그인")
+                        .foregroundColor(Color.white)
+                        .padding(.top, 20)
+                        .transition(.move(edge: .bottom).combined(with: .opacity))
+                    Divider()
+                        .overlay(Color.white)
+                        .padding(EdgeInsets(top: 20, leading: 0, bottom: 20, trailing: 0))
+                    
+                    HStack {
+                        Button(action: {}) {
+                            Image("NaverBtnIcon")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                        }
+                        Button("카카오", action: {})
+                        Button("애플", action: {})
+                        
+                    }
+                    
+                }
+
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding()
+        .background(Color("primary"))
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+                withAnimation(.easeInOut(duration: 0.5)) {
+                    splashLoading = true
+                    logoPaddingBottom = 200
+                }
+            })
+        }
+        
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
