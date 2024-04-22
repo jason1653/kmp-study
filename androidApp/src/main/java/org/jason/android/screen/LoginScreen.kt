4d4f31fb852faf6bdf222566753641572149e95b
jason <saga1654@kakao.com>
2024-04-22 15:55:19 +0900

    컴포넌트 위치 변경

--- androidApp/src/main/java/org/jason/android/screen/LoginScreen.kt
+++ androidApp/src/main/java/org/jason/android/screen/LoginScreen.kt
@@ -1,7 +1,5 @@
-package org.jason.android.screen.auth
+package org.jason.android.screen
 
-import android.content.res.Resources
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,14 +7,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -26,14 +20,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import org.jason.android.R
-import org.jason.android.SplashScreen
 import org.jason.android.widget.button.PrimaryButton
 import org.jason.android.widget.textfield.DefaultTextField
 
